9bafd566aef707344cb678600f5ffaec63e77892
Sebastian Romero <122823588+SebaRomeroX@users.noreply.github.com>
2025-03-05 18:09:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.use((req, res) => {
 
 //------------------------
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Servidor levantado en ${PORT}`)
 })
