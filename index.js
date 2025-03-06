92e0af5fde79445255f22a11bb0db455bbc4d006
Sebastian Romero <122823588+SebaRomeroX@users.noreply.github.com>
2025-03-06 17:43:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,14 @@ res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 next();
 });
+app.options('*', (req, res) => {
+res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+res.sendStatus(200);
+});
 
+//----------------------------------------------------
 
 
 app.use(express.json()) // parcea body para POST
